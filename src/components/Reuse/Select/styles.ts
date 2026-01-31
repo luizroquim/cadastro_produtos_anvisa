c5e1aff0f4216ad66e9e2208015ed674796faf39
luizroquim <luizpheliperoquim@gmail.com>
2026-01-31 13:13:44 -0300

    feat:Implementation of hookform and yup validation

--- src/components/Reuse/Select/styles.ts
+++ src/components/Reuse/Select/styles.ts
@@ -16,7 +16,8 @@ export const FieldSetForm = styled.fieldset<FieldSetFormProps>`
   background-color: ${({ theme }) => theme.colors.background};
   border-radius: 8px;
   font: ${({ theme }) => theme.font.openSans.small_600};
-  color: ${({ theme,error }) => error ? theme.colors.semantic_error: theme.colors.gray_800};
+  color: ${({ theme, error }) =>
+    error ? theme.colors.semantic_error : theme.colors.gray_800};
   padding: 8px 14px;
   border: 1px solid
     ${({ theme, error }) =>
@@ -25,8 +26,6 @@ export const FieldSetForm = styled.fieldset<FieldSetFormProps>`
     border: 1px solid
       ${({ theme, error }) =>
         error ? theme.colors.semantic_error : theme.colors.green_200};
-    box-shadow: ${({ error }) =>
-      error ? "0 0 10px 1px rgba(53, 43, 43, 0.1)" : "none"};
   }
 `;
 
--- src/pages/NewProduct/index.tsx
+++ src/pages/NewProduct/index.tsx
@@ -2,11 +2,62 @@ import { TitleContainer } from "../../components/TitleContainer";
 import { InputField } from "../../components/Reuse/InputField";
 import { Container, Content, Form, FormData, RowField } from "./styles";
 import { Select } from "../../components/Reuse/Select";
-import { useState } from "react";
 import { Button } from "../../components/Reuse/Button";
 
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+
+type FormDataProps = {
+  productName: string;
+  technicalName: string;
+  anvisaRegister: string;
+  riskClass: string;
+  status: string;
+  date?: string;
+};
+
+const schema:yup.ObjectSchema<FormDataProps> = yup.object({
+  productName: yup.string().trim().required("Nome do produto é obrigatório."),
+  technicalName: yup.string().trim().required("Nome técnico do produto é obrigatório."),
+  anvisaRegister: yup.string().trim().required("Número de registro é obrigatório."),
+  riskClass: yup.string().required("Selecione uma classe de risco."),
+  status: yup.string().required("Selecione um status de regularidade."),
+  date: yup
+    .string()
+    .optional()
+    .when("status", {
+      is: "Data de vencimento",
+      then: (schema) => schema.required("Data de vencimento é obrigatória."),
+    }),
+});
+
+
 export function NewProduct() {
-  const [status, SetStatus] = useState("");
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    defaultValues: {
+      productName: "",
+      technicalName: "",
+      anvisaRegister: "",
+      riskClass: "",
+      status: "",
+      date: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const selectedStatus = watch("status");
+
+  function onSubmit(data: FormDataProps) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Content>
@@ -15,57 +66,118 @@ export function NewProduct() {
           subtitle="Preencha os campos para adicionar um item ao catálogo"
         />
 
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <FormData>
-            <InputField placeholder="Ex: Luva descartável estéril" error={"Nome do produto  é obrigatório"}>
-              Nome do Produto:
-            </InputField>
+            <Controller
+              control={control}
+              name="productName"
+              render={({ field }) => (
+                <InputField
+                  placeholder="Ex: Luva descartável estéril"
+                  error={errors.productName?.message}
+                  {...field}
+                >
+                  Nome do Produto:
+                </InputField>
+              )}
+            />
 
-            <InputField placeholder="Ex: Luvas descartáveis">
-              Nome Técnico do Produto:
-            </InputField>
+            <Controller
+              control={control}
+              name="technicalName"
+              render={({ field }) => (
+                <InputField
+                  placeholder="Ex: Luvas descartáveis"
+                  error={errors.technicalName?.message}
+                  {...field}
+                >
+                  Nome Técnico do Produto:
+                </InputField>
+              )}
+            />
 
-            <InputField placeholder="Ex:10330660003">
-              Número do Registro da Anvisa:
-            </InputField>
+            <Controller
+              control={control}
+              name="anvisaRegister"
+              render={({ field }) => (
+                <InputField
+                  placeholder="Ex:10330660003"
+                  error={errors.anvisaRegister?.message}
+                  {...field}
+                >
+                  Número do Registro da Anvisa:
+                </InputField>
+              )}
+            />
+
             <RowField>
-              <Select
-                required
-                legend="Classe de Risco"
-                error="Classe de risco é obrigatória"
-              >
-                <option value="Classe I - Baixo Risco">
-                  Classe I - Baixo Risco
-                </option>
-                <option value="Classe II - Médio Risco">
-                  Classe II - Médio Risco
-                </option>
-                <option value="Classe III - Alto Risco">
-                  Classe III - Alto Risco
-                </option>
-              </Select>
+              <Controller
+                control={control}
+                name="riskClass"
+                render={({ field }) => (
+                  <Select
+                    legend="Classe de Risco"
+                    error={errors.riskClass?.message}
+                    {...field}
+                  >
+                    <option value="Classe I - Baixo Risco">
+                      Classe I - Baixo Risco
+                    </option>
+                    <option value="Classe II - Médio Risco">
+                      Classe II - Médio Risco
+                    </option>
+                    <option value="Classe III - Alto Risco">
+                      Classe III - Alto Risco
+                    </option>
+                  </Select>
+                )}
+              />
+              <Controller
+                control={control}
+                name="status"
+                render={({ field }) => (
+                  <Select
+                    legend="Status de Regularidade"
+                    {...field}
+                    error={errors.status?.message}
+                  >
+                    <option value="Vigente">Vigente</option>
+                    <option value="Vencido">Vencido</option>
+                    <option value="Em revalidação">Em revalidação</option>
+                    <option value="Data de vencimento">
+                      Data de vencimento
+                    </option>
+                  </Select>
+                )}
+              />
 
-              <Select
-                required
-                legend="Status de Regularidade"
-                onChange={(e) => SetStatus(e.target.value)}
-              >
-                <option value="Vigente">Vigente</option>
-                <option value="Vencido">Vencido</option>
-                <option value="Em revalidação">Em revalidação</option>
-                <option value="Data de vencimento">Data de vencimento</option>
-              </Select>
-              {status === "Data de vencimento" && (
-                <InputField error={"Data de vencimento é obrigatória"}type="date">Data de vencimento:</InputField>
+              {selectedStatus === "Data de vencimento" && (
+                <Controller
+                  control={control}
+                  name="date"
+                  render={({ field }) => (
+                    <InputField
+                      type="date"
+                      error={errors.date?.message}
+                      {...field}
+                    >
+                      Data de vencimento:
+                    </InputField>
+                  )}
+                />
               )}
             </RowField>
             <RowField>
-              
               <Button buttonColor="cancel" variant="base" isLoading={false}>
                 Cancelar
               </Button>
 
-              <Button buttonColor="primary" variant="base" isLoading={false}>
+              <Button
+                type="submit"
+                buttonColor="primary"
+                variant="base"
+                isLoading={false}
+              >
                 Cadastrar
               </Button>
             </RowField>
